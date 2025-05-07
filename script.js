3d347768cca8541838e62def2b014f16dfc720e1
Tiana-K08 <tetkononenko2020@gmail.com>
2025-05-07 10:38:47 +0200

    added arrow rotation

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const blocks = document.querySelectorAll('.question-answer');
 blocks.forEach(block => {
   const button = block.querySelector('.question-wrapper');
   const answer = block.querySelector('.answer-wrapper');
+  const questionArrow = block.querySelector('.arrow');
 
   button.addEventListener('click', () => {
-    answer.classList.toggle('close');
+    if (answer.classList.contains('close')) {
+      answer.classList.remove('close');
+      questionArrow.classList.remove('down');
+    } else {
+      answer.classList.add('close');
+      questionArrow.classList.add('down');
+    }
   });
 });
