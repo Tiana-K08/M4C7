d6bb9dd8cc41757f6c0eb2261fb2cb72202dac1d
Tiana-K08 <tetkononenko2020@gmail.com>
2025-05-07 11:14:55 +0200

    script arrow-2

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ blocks.forEach(block => {
   const button = block.querySelector('.question-wrapper');
   const answer = block.querySelector('.answer-wrapper');
   const questionArrow = block.querySelector('.arrow');
+  const answerArrow = block.querySelector('.arrow-2');
 
   button.addEventListener('click', () => {
     if (answer.classList.contains('close')) {
       answer.classList.remove('close');
       questionArrow.classList.remove('down');
     } else {
+      answer.classList.add('close');
+      questionArrow.classList.add('down');
+    }
+  });
+
+  answerArrow.addEventListener('click', () => {
+    if (!answer.classList.contains('close')) {
       answer.classList.add('close');
       questionArrow.classList.add('down');
     }
